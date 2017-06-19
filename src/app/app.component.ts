6fcbea6c145e5d9cc6da7c206f57c953e5d49033
Jurek <jurek2006@gmail.com>
2017-06-19 18:30:18 +0200

    Creating a Data Service - Sekcja 9 Wykład 98

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,31 +1,23 @@
-import { Component } from '@angular/core';
+// app.component.ts
+import { Component, OnInit } from '@angular/core';
+import { AccountsService } from './accounts.service';
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  providers: [AccountsService]
 })
-export class AppComponent {
-  accounts = [
-    {
-      name: 'Master Account',
-      status: 'active'
-    },
-    {
-      name: 'Testaccount',
-      status: 'inactive'
-    },
-    {
-      name: 'Hidden Account',
-      status: 'unknown'
-    }
-  ];
+export class AppComponent implements OnInit {
 
-  onAccountAdded(newAccount: {name: string, status: string}) {
-    this.accounts.push(newAccount);
-  }
+  accounts: {name: string, status: string}[] = [];
 
-  onStatusChanged(updateInfo: {id: number, newStatus: string}) {
-    this.accounts[updateInfo.id].status = updateInfo.newStatus;
+  constructor(private accountsService: AccountsService){}
+
+  ngOnInit(){
+    // accounts w accounts.service.ts jest tablicą, więc jest to referencja
+    // więc przez accounts mamy teraz dostęp do tej tablicy, co jest w service
+    this.accounts = this.accountsService.accounts;
   }
 }
